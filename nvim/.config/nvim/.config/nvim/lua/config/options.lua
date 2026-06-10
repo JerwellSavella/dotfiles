e6ba57c63ebc06f6adff5c001d63abcb62056e64
JerwellSavella <jerwellsavella@gmail.com>
2026-06-10 15:21:02 +0800

    added fit to screen in neovim configuration

--- nvim/.config/nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,12 @@
 -- Add any additional options here
 --
 vim.opt.clipboard = "unnamedplus"
+
+-- Wrap long lines across the screen
+vim.opt.wrap = true
+
+-- Ensure words are not broken in half if possible
+vim.opt.linebreak = true
+
+-- Indent wrapped lines to match the visual level of the parent line
+vim.opt.breakindent = true
